9f0b844dc9d2a2ecadd0d41498ed2ca9287b5b32
Andy Chioens <andy.choens@gmail.com>
2015-07-17 16:51:15 -0400

    Structure, documentation, improvements.

--- passwords.example.R
+++ passwords.example.R
@@ -1,4 +1,18 @@
+## =============================================================================
+## Passwords
+## Rename this file to passwords.R and fill in the details.
+## =============================================================================
+
+## Nighscout Host. We use mongolab ---------------------------------------------
+## This can include details like custom ports.  blah.blah.com:123456
 ns_host <- ""
-ns_pw <- ""
-ns_db <- ""
+
+## Nightscout User -------------------------------------------------------------
 ns_user <- ""
+
+## Nightscout User Password ----------------------------------------------------
+ns_pw <- ""
+
+## Nightscout Database ---------------------------------------------------------
+ns_db <- ""
+
